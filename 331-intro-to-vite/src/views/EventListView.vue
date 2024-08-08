c08e8a32a0c8b74d9e9b8b78fc4cfa446097d813
Sorod Yawa <sorod_yawa@cmu.ac.th>
2024-08-09 01:39:14 +0700

    2.10* create link to setup size of each page & size of page defined in App.vue

--- 331-intro-to-vite/src/views/EventListView.vue
+++ 331-intro-to-vite/src/views/EventListView.vue
@@ -2,29 +2,35 @@
 import EventCard from '@/components/EventCard.vue'
 import { type Event } from '@/types'
 import { ref, onMounted, computed, watchEffect } from 'vue'
+import { useRoute } from 'vue-router'
 import EventService from '@/services/EventService'
+
+const route = useRoute()
 
 const events = ref<Event[] | null>(null)
 const totalEvents = ref(0)
+const perPage = computed(() => parseInt(route.query.perPage as string) || 2)
+const page = computed(() => parseInt(route.query.page as string) || 1)
 const hasNextPage = computed(() => {
-  const totalPages = Math.ceil(totalEvents.value / 2)
+  const totalPages = Math.ceil(totalEvents.value / perPage.value)
   return page.value < totalPages
 })
 
-const props = defineProps ({
-  page: {
-    type: Number,
-    required: true
-  }
-})
-const page = computed(() => props.page)
+// const props = defineProps ({
+//   page: {
+//     type: Number,
+//     required: true
+//   }
+// })
+// const page = computed(() => props.page)
+
 onMounted ( () => {
   watchEffect(() => {
     events.value = null
-    EventService.getEvents(2, page.value)
+    EventService.getEvents(perPage.value, page.value)
       .then((response) => {
         events.value = response.data
-        totalEvents.value = response.headers['x-total-count']
+        totalEvents.value = parseInt(response.headers['x-total-count'])
       })
       .catch((error) => {
         console.error('There was an error!', error)
@@ -40,7 +46,7 @@ onMounted ( () => {
     <div class="pagination">
       <RouterLink
         id="page-prev"
-        :to="{ name: 'event-list-view', query: { page: page - 1 }}"
+        :to="{ name: 'event-list-view', query: { page: page - 1, perPage: perPage }}"
         rel="prev"
         v-if="page != 1"
         >&#60; Prev Page</RouterLink
@@ -48,7 +54,7 @@ onMounted ( () => {
 
       <RouterLink 
         id="page-next"
-        :to="{ name: 'event-list-view', query: { page: page + 1 }}"
+        :to="{ name: 'event-list-view', query: { page: page + 1, perPage: perPage }}"
         rel="next"
         v-if="hasNextPage"
         >Next Page &#62;</RouterLink
