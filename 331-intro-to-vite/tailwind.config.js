a894add8d5fc428e843c6c6b7b04675772358d7c
Sorod Yawa <sorod_yawa@cmu.ac.th>
2024-08-11 14:59:33 +0700

    10.1 added animation class

--- 331-intro-to-vite/tailwind.config.js
+++ 331-intro-to-vite/tailwind.config.js
@@ -11,7 +11,17 @@ export default {
       },
       fontFamily: {
         sans: ['Avenir', 'Helvetica', 'Arial', 'sans-serif']
-      }
+      },
+      //This is animation class
+      animation: {
+        fade: 'yellowFade 3s ease-in-out;',
+      },
+      keyframes: {
+        yellowFade: {
+          '0%': { backgroundColor: 'yellow' },
+          '100%' : { backgroundColor: 'transparent' },
+        },
+      },
     },
   },
   plugins: [],
