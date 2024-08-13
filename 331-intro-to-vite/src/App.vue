c66fa1d47a8584cbae5f911534cbdbde0d5910bd
Sorod Yawa <sorod_yawa@cmu.ac.th>
2024-08-14 00:30:48 +0700

    Edit 13.5

--- 331-intro-to-vite/src/App.vue
+++ 331-intro-to-vite/src/App.vue
@@ -2,16 +2,14 @@
 import { RouterLink, RouterView } from 'vue-router'
 import { useMessageStore } from './stores/message';
 import { storeToRefs } from 'pinia';
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { injectSpeedInsights } from '@vercel/speed-insights';
-
-injectSpeedInsights();
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const store = useMessageStore()
 const { message } = storeToRefs(store)
 </script>
 
 <template>
+  <SpeedInsights />
   <div class="text-center font-sans texe-gray-700 antialiased">
     <header>
       <div id="flashMessage" class="animate-fade" v-if="message">
